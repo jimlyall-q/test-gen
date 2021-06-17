7da7b986dd0049fc7d05cb869b45ca408e3bf05b
Jim Lyall <jim.lyall@qorvo.com>
2021-06-17 10:58:46 +0200

    More tidy

--- test_gen.py
+++ test_gen.py
@@ -17,21 +17,6 @@ def populate_tc(steps):
         print('        logger.info("' + step['test_id'] + ': ' + step['test_title'] + '")')
         print('        self.next_step()')
 
-def getnextline_on_stdin():
-    lines = stdin.readlines()
-    print(lines)
-    for line in lines:
-        line = line.strip()
-        while line == "":
-            print('reading')
-            return
-        else:
-            print(line)
-            yield line
-
-    return        
-    # line = stdin.readline()
-
 
 count = 1
 posn = 0
@@ -53,18 +38,12 @@ while line:
     if matched:
         is_new_entry = (matched.group(1) is not None) and ((matched.group(2) is not None) ==
                                              (matched.group(3) is not None))
-        # print((matched.group(1)), (matched.group(2)), (matched.group(3)))
-        # print(matched.groups(), len(matched.groups()), (matched.group(2) is not None) == (matched.group(3) is not None))
-
-    # print(line)
     if is_new_entry:
         if (count != 1):
             print(foodict)
             steps.append(foodict)
         foodict = {}
         posn = 0
-        # print(line, count)
-        # assert(int(line) == count)
         count += 1
         foodict[cols[posn]] = line
     elif line[0:1] == 'IF':
@@ -76,17 +55,11 @@ while line:
             foodict[cols[len(cols)-1]] = foodict[cols[len(cols)-1]] + '.' + line
         else:
             foodict[cols[posn]] = line
-        # print(posn, line)
 
     line = stdin.readline()
     if not line:
-        # pass
-        # print("line")
         print(foodict)
         steps.append(foodict)
 
     
 populate_tc(steps)
-
-if __name__ == '__main__':
-    print('shebango')
